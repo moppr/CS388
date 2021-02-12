28809e640a51eb4b2f0d73970b2e4004fe6b632e
moppr <mantony42@gmail.com>
2021-02-11 23:40:29 -0500

    Complete part 1 Coding

--- Clicker/app/src/main/java/com/example/clicker/MainActivity.java
+++ Clicker/app/src/main/java/com/example/clicker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.clicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,11 +12,17 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
     Button myButton;
     TextView myText;
+    int clicks = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myButton = (Button)findViewById(R.id.purplebutton);
         myText = (TextView)findViewById(R.id.textView);
+    }
+
+    public void onClick(View v){
+        clicks++;
+        myText.setText(clicks + " cookie" + (clicks == 1 ? "" : "s"));
     }
 }
