0fd4eb4a083a2ae87ec0dfafb4d4921792b99aec
moppr <mantony42@gmail.com>
2021-02-11 23:23:49 -0500

    Complete part 1 UI

--- Clicker/app/src/main/java/com/example/clicker/MainActivity.java
+++ Clicker/app/src/main/java/com/example/clicker/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.clicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button myButton;
+    TextView myText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButton = (Button)findViewById(R.id.purplebutton);
+        myText = (TextView)findViewById(R.id.textView);
     }
 }
