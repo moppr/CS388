c89ecc300e1b3920955934976b7b25973dd648c3
moppr <mantony42@gmail.com>
2021-09-20 23:31:11 -0400

    Add first stretch story (landscape/portrait)

--- Flixster/app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ Flixster/app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -68,7 +69,15 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageURL;
+            // if phone in landscape, make imageurl backdrop, otherwise poster image
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageURL = movie.getBackdropPath();
+            } else{
+                imageURL = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageURL).into(ivPoster);
         }
     }
 }
