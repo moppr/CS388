b78f07f47d449bcce2208b265425983f41a8f526
moppr <mantony42@gmail.com>
2021-09-29 23:22:48 -0400

    Add YouTube player implementation

--- Flixster/app/src/main/java/com/example/flixster/DetailActivity.java
+++ Flixster/app/src/main/java/com/example/flixster/DetailActivity.java
@@ -3,15 +3,30 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.parceler.Parcels;
 
-public class DetailActivity extends AppCompatActivity {
+import okhttp3.Headers;
 
+public class DetailActivity extends YouTubeBaseActivity {
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDVi6Jdn1injWIa2erKIkjGWag3ogoHxg4";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    YouTubePlayerView youTubePlayerView;
     TextView tvTitle2;
     TextView tvOverview2;
     RatingBar ratingBar;
@@ -24,10 +39,49 @@ public class DetailActivity extends AppCompatActivity {
         tvTitle2 = findViewById(R.id.tvTitle2);
         tvOverview2 = findViewById(R.id.tvOverview2);
         ratingBar = findViewById(R.id.ratingBar);
+        youTubePlayerView = findViewById(R.id.player);
 
         Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
         tvTitle2.setText(movie.getTitle());
         tvOverview2.setText(movie.getOverview());
         ratingBar.setRating((float) movie.getRating());
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+                    // extra: default video if no results found, instead of returning quietly
+                    String youtubeKey = results.length() == 0 ? "dQw4w9WgXcQ" : results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity", youtubeKey);
+                    initializeYoutube(youtubeKey);
+                } catch (JSONException e) {
+                    Log.e("DetailActivity", "Failed to parse JSON", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+            }
+        });
+
+    }
+
+    private void initializeYoutube(final String youtubeKey) {
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("DetailActivity", "onInitializationSuccess");
+                youTubePlayer.cueVideo(youtubeKey);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity", "onInitializationFailure");
+            }
+        });
     }
 }
--- Flixster/app/src/main/java/com/example/flixster/models/Movie.java
+++ Flixster/app/src/main/java/com/example/flixster/models/Movie.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Parcel
 public class Movie {
 
+    int movieId;
     String posterPath;
     String title;
     String overview;
@@ -26,6 +27,7 @@ public class Movie {
         overview = jsonObject.getString("overview");
         backdropPath = jsonObject.getString("backdrop_path");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -54,5 +56,9 @@ public class Movie {
 
     public double getRating() {
         return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
